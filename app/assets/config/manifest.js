d498205f0b3a8bccabf10806a0cb236d83042606
Pavel Sutugin <mr.sutugin@gmail.com>
2022-06-17 23:41:15 +0300

    StaticController

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,5 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_directory ../../../lib/assets/css .css
+//= link_directory ../../../lib/assets/javascript .js
